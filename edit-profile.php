616cf138775ba1c097f98800e63fe15ecddeb4c8
iljaschoepen <ilja.schoepen@student.thomasmore.be>
2016-03-25 17:46:17 +0100

    Worked on navbar and edit-profile.php

--- edit-profile.php
+++ edit-profile.php
@@ -1,4 +1,16 @@
-<!DOCTYPE html>
+<?php
+    include_once 'classes/User.class.php';
+    session_start();
+
+    if(isset($_SESSION['loggedin'])){
+        $user = new User();
+        $userData = $user->getAll($_SESSION['username']);
+    }else{
+        header('location: login.php');
+    }
+
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -17,19 +29,19 @@
     <h1>Edit profile</h1>
     <form action="">
         <label for="name">Name</label>
-        <input type="text" id="name" name="name">
+        <input type="text" id="name" name="name" value="<?php echo $userData['fullName']; ?>">
         
         <label for="email">Email</label>
-        <input type="email" id="email" name="email">
+        <input type="email" id="email" name="email" value="<?php echo $userData['email']; ?>">
         
         <label for="username">Username</label>
-        <input type="text" id="username" name="username">
+        <input type="text" id="username" name="username" value="<?php echo $userData['username']; ?>">
         
         <label for="biography">Biography</label>
-        <textarea name="biography" id="biography" cols="30" rows="10"></textarea>
+        <textarea name="biography" id="biography" cols="30" rows="10"><?php echo $userData['bio']; ?></textarea>
         
         <label for="website">Website</label>
-        <input type="text" id="website" name="website">
+        <input type="text" id="website" name="website" value="<?php echo $userData['website']; ?>">
         
         <input type="submit" value="Submit">
     </form>
